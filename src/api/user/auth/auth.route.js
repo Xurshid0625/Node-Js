4b49083e61e8621b43aee91283fb5da66603e4c6
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-05-10 10:42:07 +0500

    refactoring

--- src/api/user/auth/auth.route.js
+++ src/api/user/auth/auth.route.js
@@ -6,6 +6,7 @@ import {registerSchema, loginSchema} from "../../../validations/auth.validation.
 
 const router = Router();
 
+
 router.post('/register', validate(registerSchema), register);
 router.post('/login', validate(loginSchema), login);
 router.get('/me', authMiddleware, getMe);
